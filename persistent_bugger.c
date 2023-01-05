6ac2a0335ee446f79a8034f58e146c2ec13e8e47
Michael <S2210239015@students.fh-hagenberg.at>
2023-01-05 22:44:15 +0100

    finished kata

--- persistent_bugger.c
+++ persistent_bugger.c
@@ -13,16 +13,35 @@ For example (Input --> Output):
 int persistence(int n)
 {
     int ret = 0;
+    int cur_digit;
+    int res = 1;
+    int digit_cnt = 0;
 
+    while ((n / 10) != 0)
+    {
+        while (n != 0)
+        {
+            digit_cnt++;
+            cur_digit = n % 10;
+            printf("%d\n", cur_digit);
+            res *= cur_digit;
+            n /= 10;
+        }
+        n = res;
+        printf("res = %d\n", res);
+        res = 1;
+        ret++;
+    }
+    printf("\n");
     return ret;
 }
 
 int main(int argc, char **argv)
 {
-    int test = 999;
+    int test = 4;
 
     int res = persistence(test);
 
-    printf("%d\n", res);
+    printf("persistence = %d\n", res);
     return 0;
 }
